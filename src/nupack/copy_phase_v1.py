5e292830978666de9158fb6ca8e3b5f7d0d6cf4c
Rajiv <rajivpensidpri@gmail.com>
2025-09-26 23:01:05 -0400

    A full working Nupack code!

---
+++ src/nupack/copy_phase_v1.py
@@ -0,0 +1,106 @@
+import os
+
+from src.nupack import utils as nutils
+from nupack import *
+
+
+def get_data_dir():
+    data_dir = str(__file__).split('/')[-1].replace(".py", "")
+    assert os.path.exists(
+        f'./data/{data_dir}'), "Place the domains/strands/complexes in data/<file_name>"
+    assert os.path.exists(f'./data/{data_dir}/domains.csv')
+    assert os.path.exists(f'./data/{data_dir}/strands.csv')
+    assert os.path.exists(f'./data/{data_dir}/complexes.csv')
+    return data_dir
+
+
+domains_df, strands_df, complexes_df = nutils.read_data_dir(get_data_dir())
+
+domains = nutils.build_domains_from_df(domains_df)
+strands = nutils.build_target_strands_from_df(strands_df, domains)
+complexes = nutils.build_target_complexes_from_df(complexes_df, strands)
+
+# Specify the hard constraints.
+div1 = Diversity(word=4, types=2)
+div2 = Diversity(word=6, types=3)
+div3 = Diversity(word=10, types=4, scope=[nutils.extract_domain_by_name('dy1',
+                                                                        domains),
+                                          nutils.extract_domain_by_name('dy2',
+                                                                        domains),
+                                          nutils.extract_domain_by_name('dw',
+                                                                        domains)])
+
+
+# Specify the soft constraints.
+ssm1 = SSM(word=4, weight=0.3)
+ssm2 = SSM(word=5, weight=0.4)
+ssm3 = SSM(word=6, weight=0.5)
+a_t_5 = Pattern(["W5"], weight=0.5)
+
+diff1 = EnergyMatch([
+    nutils.extract_domain_by_name('dx', domains),
+    nutils.extract_domain_by_name('dx1m', domains),
+    nutils.extract_domain_by_name('dx2m', domains)
+])
+
+diff2 = EnergyMatch([
+    nutils.extract_domain_by_name('dy1', domains),
+    nutils.extract_domain_by_name('dy2', domains),
+    nutils.extract_domain_by_name('dw', domains)
+])
+
+BASE_CONC = 1e-7  # 100 nM
+concentrations = {}
+for c in complexes:
+    concentrations[c] = BASE_CONC
+
+base1 = nutils.extract_strand_by_name('base1', strands)
+base2 = nutils.extract_strand_by_name('base2', strands)
+base3 = nutils.extract_strand_by_name('base3', strands)
+sx = nutils.extract_strand_by_name('x', strands)
+sx1m = nutils.extract_strand_by_name('x1m', strands)
+sx2m = nutils.extract_strand_by_name('x2m', strands)
+sy1 = nutils.extract_strand_by_name('y1', strands)
+sy2 = nutils.extract_strand_by_name('y2', strands)
+sw = nutils.extract_strand_by_name('w', strands)
+t1 = TargetTube(on_targets=concentrations, name='t1',
+                off_targets=SetSpec(max_size=3, exclude=[[base1, sx, sy1],
+                                                         [base2, sx1m, sy2],
+                                                         [base3, sx2m, sw],
+                                                         [base1, sx],
+                                                         [base2, sx1m],
+                                                         [base3, sx2m]]))
+
+my_options = DesignOptions(f_stop=0.01, seed=93)
+
+model = Model(material='dna', celsius=37, sodium=0.5, magnesium=0.1)
+
+my_design = tube_design(tubes=[t1], model=model, options=my_options,
+                        hard_constraints=[div1, div2, div3],
+                        soft_constraints=[ssm1, ssm2, ssm3, diff1, diff2, a_t_5])
+
+results = my_design.run(trials=3)[0]
+
+print(results)
+
+strand_results = list(results.to_analysis.strands.values())
+keys = list(results.to_analysis.strands.keys())
+
+data = {}
+for strand_result in strand_results:
+    data[strand_result] = 1e-7
+
+an1 = Tube(strands=data, complexes=SetSpec(max_size=2), name='an1')
+
+tube_results = tube_analysis(tubes=[an1], model=model)
+
+print(tube_results)
+
+domain_results = results.to_analysis.domains
+
+for k, v in domain_results.items():
+    print(k.name, v)
+
+strand_results = results.to_analysis.strands
+for k, v in strand_results.items():
+    print(k.name, v)
---
+++ src/nupack/utils.py
@@ -0,0 +1,168 @@
+import re
+from typing import List
+import itertools
+import pandas as pd
+from nupack import *
+
+
+def extract_index_by_name(name: str, l: List[str]) -> int:
+    index = -1
+    for idx, elem in enumerate(l):
+        if elem == name:
+            index = idx
+            break
+    assert index != -1, "Name not found!"
+    return index
+
+
+def extract_strand_by_name(name: str, strands: List[TargetStrand]):
+    names = [strand.name for strand in strands]
+    return strands[extract_index_by_name(name, names)]
+
+
+def extract_domain_by_name(name: str, domains: List[Domain]):
+    names = [d.name for d in domains]
+    return domains[extract_index_by_name(name, names)]
+
+
+def extract_complex_by_name(name: str, complexes: List[Complex]):
+    names = [c.name for c in complexes]
+    return complexes[extract_index_by_name(name, names)]
+
+
+def read_lines(filepath):
+    lines = []
+    with open(filepath, 'r') as fin:
+        lines = fin.readlines()
+        lines = [line.strip() for line in lines]
+    return lines
+
+
+def read_data_dir(data_dir: str, sep: str = '\t') -> [pd.DataFrame] * 3:
+    domains_csv = os.path.join('data', data_dir, 'domains.csv')
+    domains_df = pd.read_csv(domains_csv, sep=sep)
+
+    strands_csv = os.path.join('data', data_dir, 'strands.csv')
+    strands_df = pd.read_csv(strands_csv, sep=sep)
+
+    complexes_csv = os.path.join('data', data_dir, 'complexes.csv')
+    complexes_df = pd.read_csv(complexes_csv, sep=sep)
+
+    return domains_df, strands_df, complexes_df
+
+
+def create_domain(name: str, code: str) -> Domain:
+    """code must be acceptable.
+    Code	Nucleotides
+M	A or C
+R	A or G
+W	A or U
+S	C or G
+Y	C or U
+K	G or U
+V	A, C, or G
+H	A, C, or U
+D	A, G, or U
+B	C, G, or U
+N	A, C, G, or U
+For DNA, T replaces U.
+    """
+    assert re.match('([MRWSYKVHDBNATGCU][0-9]+)*', code)
+    d = Domain(code, name=name)
+    return d
+
+
+def build_domains_from_df(df) -> List[Domain]:
+    domains = [create_domain(name, code) for name, code in
+               zip(df.name, df.code)]
+    return domains
+
+
+def create_target_strand(name: str, domains_raw: str, domains: List[Domain],
+                         sep=',') -> TargetStrand:
+    """code must be acceptable.
+    Code	Nucleotides
+M	A or C
+R	A or G
+W	A or U
+S	C or G
+Y	C or U
+K	G or U
+V	A, C, or G
+H	A, C, or U
+D	A, G, or U
+B	C, G, or U
+N	A, C, G, or U
+For DNA, T replaces U.
+    """
+    strand_domains = []
+    all_domains = [d.name for d in domains]
+    strand_domains_list = domains_raw.split(sep)
+
+    for domain_str in strand_domains_list:
+        revcompFlag = False
+        if '~' in domain_str:
+            revcompFlag = True
+        domain_str = domain_str.replace('~', '')
+
+        # Extract the index of the domain in the total domains list.
+        index = extract_index_by_name(domain_str, all_domains)
+
+        if revcompFlag:
+            strand_domains.append(~domains[index])
+        else:
+            strand_domains.append(domains[index])
+    s = TargetStrand(strand_domains, name=name)
+    return s
+
+
+def build_target_strands_from_df(df, domains=[]):
+    assert len(domains) > 0, "Domains empty!"
+    strands = [
+        create_target_strand(name, domains_raw, domains)
+        for name, domains_raw in zip(df.name, df.domains)
+    ]
+    return strands
+
+
+def create_target_complex(name: str, strands_raw: str, code: str,
+                          strands: List[TargetStrand],
+                          sep=',') -> TargetComplex:
+    all_strand_names = [strand.name for strand in strands]
+    complex_strand_names = strands_raw.split(sep)
+    complex_strands = []
+
+    for sname in complex_strand_names:
+        index = extract_index_by_name(sname, all_strand_names)
+        complex_strands.append(strands[index])
+
+    complex = TargetComplex(complex_strands, code, name=name)
+    return complex
+
+
+def build_target_complexes_from_df(df, strands: List[Strand] = []) -> \
+        List[TargetComplex]:
+    complexes = [
+        create_target_complex(name, strands_raw, code, strands)
+        for name, strands_raw, code in zip(df.name, df.strands, df.code)
+    ]
+    return complexes
+
+
+def get_complement_domain_name(name: str) -> str:
+    if '*' in name:
+        revcomp = name.replace('*', '')
+        return revcomp
+    else:
+        revcomp = name + '*'
+        return revcomp
+
+
+# def get_offtarget_exclude_complexes(strands, max_size=3):
+#     """Exclude a complex if one encompasses the others."""
+#     exclude_complexes = []
+#     t = tuple([strands]*max_size)
+#     tuples = list(itertools.product(*t))
+#     for t in tuples:
+#
+#     return
